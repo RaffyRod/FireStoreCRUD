05b841efb2b02df22c864ffe5481547b35b5f7d7
Raffy Anyelo <elraffy3@gmail.com>
2020-06-02 00:38:01 -0400

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { NuevaRecetaComponent } from './components/nueva-receta/nueva-receta.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { AboutComponent } from './components/about/about.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { DetailsComponent } from './components/details/details.component';
+import { EditComponent } from './components/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    NuevaRecetaComponent,
+    AdminComponent,
+    AboutComponent,
+    NotFoundComponent,
+    DetailsComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
